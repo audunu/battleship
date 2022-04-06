7d79873f1992ae638fd0c9a8280acfd432f0d08b
audunu <audunu@gmail.com>
2022-04-06 12:33:53 +0200

    start over

--- script.js
+++ script.js
@@ -1,57 +1,73 @@
-//let myGridSize = prompt('what size would you like your grid to be?');
-//let enemyGridSize = prompt('what size would you like your enemy grid to be?');
+const gridContainer = document.querySelector('.grid-container')
+const shipContainer = document.querySelector('.ships')
 
-let gameboardSize = 10;
-let gameboard = [];
+const gameboard1 = createGameboard().gameboard
 
-function shipFactory(size) {
-    const shipLength = Math.floor(Math.random() * size) + 1;
-    const sunk = false;
-    const hits = [];
-    for (let i = 0; i < shipLength; i++) {
-        hits[i] = 'o';
+const destroyer = createShip(5);
+const speedboat = createShip(2);
+const attacker = createShip(3);
+
+function showShips() {
+    const destroyer = document.createElement('div');
+    destroyer.innerText = 'Place your destroyer 5 units long'
+    shipContainer.appendChild(destroyer)
+}
+
+showShips()
+
+function placeShip(event) {
+    const firstCoordinate = event.target.attributes.getNamedItem('data-index').value[0]
+    const secondCoordinate = event.target.attributes.getNamedItem('data-index').value[2]
+    gameboard1[firstCoordinate][secondCoordinate] = destroyer.ship[0]
+    renderGameboard();
+}
+
+
+
+function createShip(length) {
+    const ship = []
+    for (i = 0; i < length; i++) {
+        ship[i] = 'o'
     }
-
     return {
-        shipLength,
-        sunk,
-        hits
+        ship: ship,
+        isSunk: false
+    }
+}
+
+function createGameboard() {
+    const gameboard = [
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', ''],
+    ]
+    return {
+        gameboard: gameboard
     }
 }
 
 
-function placeShip(ship) {
-    const startCoordinateOne = gameboardSize - Math.floor(Math.random() * ship.shipLength) - 1;
-    const endCoordinateOne = startCoordinateOne + ship.shipLength - 1;
-    const coordinateTwo = Math.floor(Math.random() * gameboardSize) - 1;
-    const determineDirection = () => Math.floor(Math.random() * 2 + 1);
-    // if vertical placement of ship
-    if (determineDirection === 1) {
-        for (let i = startCoordinateOne; i <= endCoordinateOne; i++) {
-            gameboard[i][coordinateTwo] = 'o';
-        }
-    }
-    else if (determineDirection === 2) {
-        for (let i = startCoordinateOne; i <= endCoordinateOne; i++) {
-            gameboard[coordinateTwo][i] = 'o';
 
-        }
-    }
-    return gameboard;
-
+function renderGameboard() {
+    gridContainer.innerHTML = '';
+    gameboard1.forEach((arrayelement, iindex) => arrayelement.forEach((subarrayelement, jindex) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerText = subarrayelement
+        square.setAttribute('data-index', iindex + '-' + jindex)
+        square.addEventListener('click', placeShip)
+        gridContainer.appendChild(square)        
+    }))
 }
 
+renderGameboard();
 
-const createGameboard = (size) => {
-    let gameboard = [];
-    for (let i = 0; i < size; i++) {
-        gameboard[i] = [];
-        for (let j = 0; j < size; j++) {
-            gameboard[i][j] = '-';
-        }
-    }
-    return gameboard;
-}
 
-const ship1 = shipFactory(gameboardSize);
-gameboard = createGameboard(gameboardSize);
+ 
